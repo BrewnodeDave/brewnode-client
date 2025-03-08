75012cef50e5f570d27bdc7ee4b9bf3cc7134a18
Brewnode Dave <dave@brewnode.co.uk>
2025-03-08 17:35:32 +0000

    layout

--- src/pages/Devices.jsx
+++ src/pages/Devices.jsx
@@ -7,101 +7,105 @@ import Toggle from '../common/Toggle.jsx';
 
 const Devices = (props) => {  
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
-      <Container sx={{ height: '100%' , width: '100%'}}>
-        <Grid container xs={12} sx={{ height: '100%' }}> 
-          <Grid xs={4} sx={{ padding: 1, height: '100%' }}>
+    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <Container sx={{ height: '100%' , width: '100%' }}>
+        <Grid container sx={{ height: '100%' }}>
+          {/* column 1 */}
+          <Grid item xs={12} md={4} sx={{ padding: 1, height: '100%' }}>
             <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '100%' }}>
               <h1>Kettle</h1> 
               <Temperature name='Kettle' sensor='TempKettle' min={0} max={100} tooLow={30} low={40} ok={75}/>
-              <Grid container xs={12}>
-                <Grid xs={6}>    
+              <Grid container>
+                <Grid item xs={6}>    
                   <Toggle sensorName="ValveKettleIn" displayName="Valve"/>
                 </Grid>
-                <Grid xs={6}>    
+                <Grid item xs={6}>    
                   <Toggle sensorName="Fan" displayName="Fan"/>
                 </Grid>
               </Grid>
-              <Grid container xs={12}>    
-                <Grid xs={6}>    
+              <Grid container>    
+                <Grid item xs={6}>    
                   <Toggle sensorName="PumpKettle" displayName="Pump"/>
                 </Grid>
-                <Grid xs={6}>    
+                <Grid item xs={6}>    
                   <Toggle sensorName="Heater" displayName="Heater"/>
                 </Grid>
               </Grid>
             </Box> 
           </Grid>  
 
-          <Grid xs={4} sx={{ padding: 1, height: '100%' , width: '100%'}}>
+          {/* column 2 */}
+          <Grid item xs={12} md={4} sx={{ padding: 1, height: '100%' }}>
             <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '25%' }}>
-              <Grid container xs={12}>    
-                <Grid xs={6}>
+              <Grid container>    
+                <Grid item xs={6}>
                   <h1>Fermenter</h1>
                 </Grid>
-                <Grid xs={6}>
+                <Grid item xs={6}>
                   <Temperature name='Ferment' sensor='TempFermenter' min={5} max={40} tooLow={10} low={15} ok={21} high={25}/>
                 </Grid>
               </Grid>
             </Box>
-            <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '75%' }}>
-              <Grid container xs={12}>    
-                <Grid xs={6}>
+
+            <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '75%' }}>
+              <Grid container>    
+                <Grid item xs={6}>
                   <h1>Glycol</h1>
                 </Grid>
-                <Grid xs={6}>
+                <Grid item xs={6}>
                   <Temperature name='Glycol' sensor='TempGlycol' min={-10} max={40} tooLow={-10} low={0} ok={10} high={30}/>
                 </Grid>
               </Grid>
-              <Grid container xs={12}>    
-                <Grid xs={6}>
+              <Grid container>    
+                <Grid item xs={6}>
                   <Toggle sensorName="PumpGlycol" displayName="Pump"/>
                 </Grid>
-                <Grid xs={6}>
+                <Grid item xs={6}>
                   <Toggle sensorName="GlycolHeater" displayName="Heat"/>
                 </Grid>
               </Grid>
-              <Grid container xs={12}>    
-                <Grid xs={12}>
+              <Grid container>    
+                <Grid item xs={12}>
                   <Toggle sensorName="GlycolChiller" displayName="Chill"/>
                 </Grid>
               </Grid>
             </Box>
           </Grid>
 
-          <Grid xs={4} sx={{ padding: 1, height: '100%' , width: '100%'}}>
+          {/* column 3 */}
+          <Grid item xs={12} md={4} sx={{ padding: 1, height: '100%' }}>
             <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '50%' }}>
-              <Grid container xs={12}>    
-                <Grid xs={6}>
+              <Grid container>    
+                <Grid item xs={6}>
                   <h1>Mash Tun</h1>
                 </Grid>
-                <Grid xs={6}>
+                <Grid item xs={6}>
                   <Temperature name="Mash" sensor='TempMash' min={50} max={80} tooLow={55} low={60} ok={70} high={75}/>
                 </Grid>
               </Grid>
-              <Grid container xs={12}>    
-                <Grid xs={6}>
+              <Grid container>    
+                <Grid item xs={6}>
                   <Toggle sensorName="ValveMashIn" displayName="Valve"/>
                 </Grid>
-                <Grid xs={6}>
+                <Grid item xs={6}>
                   <Toggle sensorName="PumpMash" displayName="Pump"/>
                 </Grid>
               </Grid>
             </Box>
             <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '50%' }}>
-              <Grid container xs={12}>    
-                <Grid xs={6}>
+              <Grid container>    
+                <Grid item xs={6}>
                   <h1>Chiller</h1>
                 </Grid>
-                <Grid xs={6}>
+                <Grid item xs={6}>
                   <Temperature name="Ambient" sensor='TempAmbient'/>
                 </Grid>
               </Grid>
-              <Grid container xs={12}>    
-                <Grid xs={6}>
+              <Grid container>    
+                <Grid item xs={6}>
                   <Toggle sensorName="ValveChillWortIn" displayName="Input Valve"/>
                 </Grid>
-                <Grid xs={6}>
+                <Grid item xs={6}>
                   <Toggle sensorName="ValveFermentIn" displayName="Output Valve"/>
                 </Grid>
               </Grid>
