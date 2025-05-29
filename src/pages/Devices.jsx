842115f00f336ab8d39b96d5874aec270b45e225
Brewnode Dave <dave@brewnode.co.uk>
2025-05-29 15:06:41 +0100

    devices tab alignment

--- src/pages/Devices.jsx
+++ src/pages/Devices.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Temperature from '../brewnode/Temperature.jsx';
 import Toggle from '../common/Toggle.jsx';
 
 const Devices = (props) => {  
   return (
-    <div style={{ width: '100%', height: '100%', margin: 0, padding: 0 }}>
-      <Container sx={{ height: '100%', width: '100%', maxWidth: '100vw !important', padding: 0 }}>
-        <Grid container sx={{ height: '100%', alignItems: 'stretch' }}>
-          {/* column 1 */}
-          <Grid item xs={12} md={4} sx={{ padding: 1, height: '100%' }}>
-            <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '100%' }}>
+    <Grid container>
+      <Grid container spacing={2}  margin={2} xs={12}>
+        
+        <Grid container sx={12}>
+          {/* Kettle */}
+          <Grid item xs={4}>
+            <Box sx={{ margin: 1, border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '100%' }}>
               <Grid container>    
                 <Grid item xs={6}>
                   <h1>Kettle</h1>
@@ -42,9 +42,9 @@ const Devices = (props) => {
             </Box> 
           </Grid>  
 
-          {/* column 2 */}
-          <Grid item xs={12} md={4} sx={{ padding: 1, height: '100%' }}>
-            <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '10%' }}>
+          {/* Fermenter and Glycol */}
+          <Grid xs={4}>
+            <Box sx={{margin: 1, border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '10%' }}>
               <Grid container>    
                 <Grid item xs={6}>
                   <h1>Fermenter</h1>
@@ -55,7 +55,7 @@ const Devices = (props) => {
               </Grid>
             </Box>
 
-            <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '88%' }}>
+            <Box sx={{ margin: 1, border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '88%' }}>
               <Grid container>    
                 <Grid item xs={6}>
                   <h1>Glycol</h1>
@@ -82,9 +82,9 @@ const Devices = (props) => {
             </Box>
           </Grid>
 
-          {/* column 3 */}
-          <Grid item xs={12} md={4} sx={{ padding: 1, height: '100%' }}>
-            <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '48%' }}>
+          {/* Mash Tun and Chiller */}
+          <Grid xs={4}>
+            <Box sx={{ margin: 1, border: 2, bgcolor: '#8bb34a', marginBottom: 2, height: '48%' }}>
               <Grid container>    
                 <Grid item xs={6}>
                   <h1>Mash Tun</h1>
@@ -103,7 +103,7 @@ const Devices = (props) => {
               </Grid>
             </Box>
 
-            <Box sx={{ border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '48%' }}>
+            <Box sx={{ margin: 1, border: 2, bgcolor: '#8bb34a', marginBottom: 0, height: '48%' }}>
               <Grid container>    
                 <Grid item xs={6}>
                   <h1>Chiller</h1>
@@ -125,9 +125,9 @@ const Devices = (props) => {
 
           </Grid>
         </Grid>
-      </Container>
-    </div>
-  );
+      </Grid>
+    </Grid>
+    );
 };
 
 export { Devices };
